f3b4b5863ec37fbca7bfcfef7f4bf88a120551ee
Stepan Sendun <Stepan_Sendun@epam.com>
2024-03-21 10:28:53 +0200

    broadcast message

--- http-service/chat/chat-handler.go
+++ http-service/chat/chat-handler.go
@@ -1,28 +1,56 @@
 package chat
 
 import (
-	"io"
 	"log"
 
 	"golang.org/x/net/websocket"
 )
 
-type chatHandlers struct{}
+type chatHandlers struct {
+	connectionsByUser map[string]*websocket.Conn
+}
 
 func NewChatHandlers() *chatHandlers {
-	return &chatHandlers{}
+	return &chatHandlers{
+		connectionsByUser: make(map[string]*websocket.Conn),
+	}
 }
 
 func (h *chatHandlers) Connect(ws *websocket.Conn) {
+	// ask how this casting works
+	user := ws.Request().Context().Value("user").(string)
+	h.connectionsByUser[user] = ws
+
 	defer func() {
 		err := ws.Close()
+		delete(h.connectionsByUser, user)
 		if err != nil {
 			log.Println("Error closing websocket:", err)
 		}
 	}()
 
-	_, err := io.Copy(ws, ws)
-	if err != nil {
-		log.Println("Error copying data:", err)
+	for {
+		var message string
+		err := websocket.Message.Receive(ws, &message)
+		if err != nil {
+			log.Println("Error receiving message:", err)
+			break
+		}
+		h.fanOutMessage(user, message)
 	}
+}
+
+func (h *chatHandlers) fanOutMessage(user string, message string) {
+	for u, ws := range h.connectionsByUser {
+		if u != user {
+      go h.sendMessageTo(ws, user, message)
+		}
+	}
+}
+
+func (h *chatHandlers) sendMessageTo(ws *websocket.Conn, user string, message string) {
+  err := websocket.Message.Send(ws, message)
+  if err != nil {
+    log.Println("Error sending message:", err)
+  }
 }
